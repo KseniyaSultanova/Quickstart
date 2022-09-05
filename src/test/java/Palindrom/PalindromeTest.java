b04315fb904e4a20c26d323dcb67585f1ff2ec06
KseniyaSultanova <60780721+KseniyaSultanova@users.noreply.github.com>
2022-09-05 19:56:04 +0300

    Lesson 3

--- src/test/java/Palindrom/PalindromeTest.java
+++ src/test/java/Palindrom/PalindromeTest.java
@@ -1,6 +1,7 @@
 package Palindrom;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -9,20 +10,22 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class PalindromeTest {
     Palindrome palindrome = new Palindrome();
 
-
-@ParameterizedTest
-@ValueSource(strings = {"123321, MOM"})
-void TestIsPalindromeMethod(){
-    Assertions.assertTrue(isPalindrome("01233210"));
+    @DisplayName("Слово является палиндромом")
+    @ParameterizedTest
+    @ValueSource(strings = {"123321, MOM"})
+    public void TestIsPalindromeMethod(){
+    Assertions.assertTrue(palindrome.isPalindrome("01233210"));
+    Assertions.assertTrue(palindrome.isPalindrome("0123A210"));
 }
 
-@Test
-boolean isPalindrome(String args) {
- Assertions.assertTrue(isPalindrome("123321"));
-    Assertions.assertTrue(isPalindrome("01233210"));
-    Assertions.assertTrue(isPalindrome("1233210"));
-    return false;
-}
+
+    @DisplayName("Слово является палиндромом")
+    @ParameterizedTest
+    @ValueSource(strings = { "racecar", "rAdar", "able was I ere I saw elba" })
+    public void isPalindromeTest(String word){
+        Assertions.assertTrue(palindrome.isPalindrome(word));
+    }
+
 }
 
 
