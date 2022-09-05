541c0fc13bc19aeea91416de45b18b973ff47361
KseniyaSultanova <60780721+KseniyaSultanova@users.noreply.github.com>
2022-09-05 19:28:39 +0300

    Lesson 3

---
+++ src/main/java/Palindrom/Palindrome.java
@@ -0,0 +1,17 @@
+package Palindrom;
+
+public class Palindrome {
+    public static void main(String[] args){
+        String s = "MOM";
+        System.out.println(isPalindrome(s) ? "YES" : "NO");
+    }
+    static boolean isPalindrome(String s) {
+
+        if (s.length() == 1 | s.length() == 0)
+            return true;
+
+        if (s.charAt(0) == s.charAt(s.length() - 1))
+            return isPalindrome(s.substring(1, s.length() - 1));
+            return false;
+    }
+}
